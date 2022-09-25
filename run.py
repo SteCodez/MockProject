61befa900fe888994809a2bc02cd5e7574efab01
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-25 02:56:49 +0100

    nter the commit message for your changes. Lines starting

--- run.py
+++ run.py
@@ -23,6 +23,21 @@ def get_sales_data():
     data_str = input("Enter your data here:")
     print(f"The data provided is {data_str}")
  
-    sales_string = data_str.split(",")
-    print(sales_data)
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+    
+def validate_data(values):
+    """
+    Inside the try coverts all string values to intergers. Raises ValueError if strings
+    cannot be converted into int,
+    or if there arent exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+    
 get_sales_data()

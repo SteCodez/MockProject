a916fc179d1b58735d3f3d99f86de972f18f8ae9
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-25 18:53:35 +0100

    update

--- run.py
+++ run.py
@@ -77,6 +77,18 @@ def calculate_surplus_data(sales_row):
    
     return surplus_data
         
+def get_last_5_entries_sales():
+
+    sales = SHEET.worksheet("sales") 
+    
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+    
+      
 def main():
     
     """
@@ -89,4 +101,7 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
    
 print("Welcome to love sandwiches Data Automation") 
+
 main()
+
+sales_columns = get_last_5_entries_sales()
